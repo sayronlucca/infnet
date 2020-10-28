339279d904adc749563c49490ce9fa98052878e5
sayronlucca <sayron.lucca@opens.com.br>
2020-10-28 12:37:42 -0300

    exercicio de funcao

--- funcao.py
+++ funcao.py
@@ -1,1 +1,9 @@
-def  quadrado()
+# Uma função que retorna o quadrado de um número:
+def quadrado(n):
+  q = n * n
+  return q
+
+# A função quadrado produz um retorno, e portanto pode ser usada como resultado de uma atribuição:
+n = float(input("Informe um número: "))
+q = quadrado(n) 
+print(f"O quadrado de {n} é {q}.")
