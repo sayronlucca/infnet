abe315b2007057132506f96568c49ede1ffd48da
sayronlucca <sayron.lucca@opens.com.br>
2020-10-28 23:49:48 -0300

    aula de condição

---
+++ condicao.py
@@ -0,0 +1,11 @@
+idade = int(input("Qual é a sua idade? "))
+
+# Cláusula if (se)
+if idade >= 18 :
+  print("Maior de idade.")
+  print("Passa a ter obrigatoriedade de voto.") 
+  print("Se brasileiro do sexo masculino, deve se alistar.")
+  print("Passa a poder responder criminalmente.")
+else:
+    print("menor de idade")
+print("Fim do programa.")
