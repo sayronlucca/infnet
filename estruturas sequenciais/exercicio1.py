9d4d26e1ac8d18b26222ca0c79bbce35245a7f62
sayronlucca <sayron.lucca@opens.com.br>
2020-11-10 01:04:24 -0300

    Adiçao exercicios 1 e 2

---
+++ estruturas sequenciais/exercicio1.py
@@ -0,0 +1,14 @@
+#Crie um algoritmo que receba, como entrada, o valor de três notas de um aluno - com valor entre 0 e 10, e, em seguida, informe a média entre elas. 
+
+
+
+n1=int(input('informe a primeira nota:'))
+
+
+n2=int(input('informe a segunda nota:'))
+
+
+n3=int(input('informe a terceira nota:'))
+
+
+print(f"a média do aluno é: {(n1+n2+n3)/3}")
---
+++ estruturas sequenciais/exercicio2.py
@@ -0,0 +1,16 @@
+#Recrie o algoritmo de cálculo de média das notas, mas, desta vez, calcule a média ponderada
+# Sabendo que a primeira nota possui peso 1, a segunda nota possui peso 2 e a terceira nota possui peso 3.
+
+
+n1=int(input('Informe a primeira nota:'))
+p1=int(input('Informe o peso1:'))
+
+
+n2=int(input('Informe a segunda nota:'))
+p2=int(input('Informe o peso2:'))
+
+
+n3=int(input('Informe a terceira nota:'))
+p3=int(input('Informe o peso3:'))
+
+print(f"a média ponderada é:{(n1*p1+n2*p2+n3*p3)/(p1+p2+p3)}")
