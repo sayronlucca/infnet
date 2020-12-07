9847ef2c24fbdafab32a2c2f98500e8191931309
sayronlucca <sayron.lucca@opens.com.br>
2020-12-07 17:11:40 -0300

    Adição questão 3 do Assessment

---
+++ Assessment/questao3.py
@@ -0,0 +1,20 @@
+#Crie um programa contendo uma função que, dados um valor de renda mensal total, gastos totais com moradia, 
+# gastos totais com educação e gastos totais com transporte, faça um diagnóstico da saúde financeira do usuário,
+# com base nos valores percentuais acima expostos, informando qual é o percentual da renda mensal total comprometido por cada custo. 
+#Se o gasto estiver dentro do percentual recomendado, informe ainda que 
+#Seus gastos estão dentro da margem recomendada.
+#Caso contrário, informe:
+#Portanto, idealmente, o máximo de sua renda comprometida com {tipo} deveria ser de R$ {valor_max}
+#Onde tipo deve ser moradia, educação ou transportes e valor_max deve ser o valor equivalente ao percentual máximo de gasto com esse tipo de custo.
+#Exemplo de saída do programa:
+#Renda mensal total: R$ 5000
+#Gastos totais com moradia: R$ 1760
+#Gastos totais com educação: R$ 800
+#Gastos totais com transporte: R$ 300
+#Diagnóstico:
+#Seus gastos totais com moradia comprometem 35.2% de sua renda total. O máximo recomendado é de 30%. Portanto, idealmente, o máximo de sua renda comprometida com moradia deveria ser de R$1500.
+#Seus gastos totais com educação comprometem 16% de sua renda total. O máximo recomendado é de 20%. Seus gastos estão dentro da margem recomendada.
+#Seus gastos totais com transporte comprometem 6% de sua renda total. O máximo recomendado é de 15%. Seus gastos estão dentro da margem recomendada.
+
+
+
